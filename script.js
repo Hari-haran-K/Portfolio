2173f163d32eed2a9a172016676dc46dd93e84e8
HariHaran <128683172+Hari-haran-K@users.noreply.github.com>
2023-08-22 18:59:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let navbar = document.querySelector("ul");
+let bar = document.querySelector(".bar .ion-icon");
+
+bar.addEventListener('click',function(){
+    navbar.classList.toggle("show")
+})
+
